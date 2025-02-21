5f83ccb7e84f72969d4569afea5cd214abba2d0c
VICTOR SILVA <victor.abelino7@gmail.com>
2025-02-20 22:38:10 -0300

    layout

--- pw3-somativa/src/App.jsx
+++ pw3-somativa/src/App.jsx
@@ -1,37 +1,13 @@
 import './App.css'
 
-import BookCard from './components/BookCard'
-
-import capa_livro from './assets/cavernas_aco.jpg'
+import NavBar from './components/Layot/NavBar'
 
 function App() {
   
   return (
     <>
       <div>
-
-        <h1>Pw3 - WEBAPP - LIVRARIA</h1>
-
-        <BookCard 
-        titulo='As Cavernas de Aço' 
-        autor='Isaac Azimov'
-        imagem={capa_livro}/>
-
-        {/* <BookCard 
-        titulo='Sol Desvelado' 
-        autor='Isaac Azimov'
-        imagem='Uma imagem vai aparecer aqui'/>
-
-        <BookCard 
-        titulo='O Fim da Infancia' 
-        autor='Arthur C. Clark'
-        imagem='Uma imagem vai aparecer aqui'/>
-
-         <BookCard 
-        titulo='Neuromancer' 
-        autor='Willian Gibson'
-        imagem='Uma imagem vai aparecer aqui'/> */}
-      
+      <NavBar/>
       </div>
     </>
   )
---
+++ pw3-somativa/src/components/Layot/NavBar.jsx
@@ -0,0 +1,15 @@
+import style from './Navbar.module.css'
+
+const Navbar = ()=>{
+    return(
+        <nav>
+            <ul>
+                <li>HOME</li>
+                <li>CADASTRAR LIVROS</li>
+                <li>LISTA LIVROS</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar ()
